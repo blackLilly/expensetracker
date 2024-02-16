a92093ce677847f7ba1313548781ac76a918fa9e
Nandhagopal R <49095039+blackLilly@users.noreply.github.com>
2024-02-16 18:42:55 +0530

    updated path
    updated path

---
+++ precache-manifest.326e0f2bb2ed3fcce61f70873ba8af5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea40ca718809ab2d525230ea704a7da1",
+    "url": "/expensetracker/index.html"
+  },
+  {
+    "revision": "69648eb3e49829eeccd2",
+    "url": "/expensetracker/static/css/2.7ac39a5d.chunk.css"
+  },
+  {
+    "revision": "fd8ca394c9043c77cb1c",
+    "url": "/expensetracker/static/css/main.6771c0cf.chunk.css"
+  },
+  {
+    "revision": "69648eb3e49829eeccd2",
+    "url": "/expensetracker/static/js/2.0376e564.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/expensetracker/static/js/2.0376e564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8ca394c9043c77cb1c",
+    "url": "/expensetracker/static/js/main.407fdf9c.chunk.js"
+  },
+  {
+    "revision": "c810f9a986d11d36bbfd",
+    "url": "/expensetracker/static/js/runtime-main.ba288cc2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5587ef03585ca6f64696892dc55b3aa1.js"
+  "/expensetracker/precache-manifest.326e0f2bb2ed3fcce61f70873ba8af5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/expensetracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
